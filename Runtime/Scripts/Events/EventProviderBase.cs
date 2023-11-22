48913adfb3e26613c58c4762236b7155e5811728
ligofff <liga@ligofff.ru>
2023-11-22 10:06:02 +0300

    EventProviderBase added

---
+++ Runtime/Scripts/Events/EventProviderBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ligofff.SAPC.Events
+{
+    public class EventProviderBase
+    {
+        [Serializable]
+        public abstract class EventSourceProviderBase<TTrg, TObs>
+        {
+            public event Action<TTrg, TObs> Open;
+
+            protected bool subscribed = false;
+
+            public void SetupProvider()
+            {
+                SetupProviderInternal();
+                subscribed = true;
+            }
+
+            public void DestroyProvider()
+            {
+                if (!subscribed) return;
+            
+                DestroyProviderInternal();
+                subscribed = false;
+            }
+
+            protected abstract void SetupProviderInternal();
+            protected abstract void DestroyProviderInternal();
+
+            protected void InvokeOpen(TTrg target, TObs observer)
+            {
+                Open?.Invoke(target, observer);
+            }
+        }
+    }
+}
